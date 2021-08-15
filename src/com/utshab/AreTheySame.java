9d0e8e6f1ecc82b71a023824ab12399345beaad1
Utshab Roy <utshab.roy@gmail.com>
2021-08-15 20:44:39 +0900

    are they same solved with sorting

--- src/com/utshab/AreTheySame.java
+++ src/com/utshab/AreTheySame.java
@@ -7,48 +7,34 @@ public class AreTheySame {
      * problem link
      * https://www.codewars.com/kata/550498447451fbbd7600041c
      * REMARK: one test failed
+     *
      * @param a
      * @param b
      * @return
      */
     public static boolean comp(int[] a, int[] b) {
-        if (a == null || b == null )
+        if (a == null || b == null)
             return false;
-        if (a.length != b.length )
+        if (a.length != b.length)
             return false;
         if (a.length == 0 && b.length == 0)
             return true;
 
-        boolean flag = true;
-        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Math.abs(a[i]) * Math.abs(a[i]);
+        }
 
         for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if (b[i] == a[j] * a[j]){
-                    count = 0;
-
-                    a[j] = -1; // I guess need to improve this section
-                    break;
-                }else{
-                    count++;
-                }
-            }
-            if (count == b.length)
-                flag = false;
+            b[i] = Math.abs(b[i]);
         }
-        return flag;
 
-//        Arrays.sort(a);
-//        Arrays.sort(b);
-//        boolean flag = true;
-//        for (int i = 0; i < a.length; i++) {
-//            if (a[i] * a[i] != b[i])
-//                flag = false;
-//        }
-//        return flag;
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
     }
 
     public static void main(String[] args) {
-        comp(new int[]{3,1,2}, new int[]{1,9, 4});
+        comp(new int[]{3, 1, 2}, new int[]{1, 9, 4});
     }
 }
