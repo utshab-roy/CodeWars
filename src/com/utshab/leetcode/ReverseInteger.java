6779cbf1cb2f3275a8e7d0b4647d809ff9629be8
Utshab Roy <utshab.roy@gmail.com>
2021-09-06 02:18:54 +0900

    reverse integer problem solved ( maybe it is not optimized )

---
+++ src/com/utshab/leetcode/ReverseInteger.java
@@ -0,0 +1,33 @@
+package com.utshab.leetcode;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        boolean negative = false;
+        if (x < 0) {
+            negative = true;
+            x = Math.abs(x);
+        }
+
+        String str = "";
+        while (x > 0) {
+            int a = x % 10;
+            x = x / 10;
+            str = str + a;
+        }
+
+        try {
+            int result = Integer.parseInt(str);
+            if (negative){
+                result *= -1;
+            }
+            return result;
+        }catch (Exception e){
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int res = new ReverseInteger().reverse(-1234);
+        System.out.println(res);
+    }
+}
