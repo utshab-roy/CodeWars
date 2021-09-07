938a50c0eec9e54fc67dbc615169c3ad70158ac9
Utshab Roy <utshab.roy@gmail.com>
2021-09-07 13:52:09 +0900

    digital root problem solved

---
+++ src/com/utshab/DigitalRoot.java
@@ -0,0 +1,28 @@
+package com.utshab;
+
+public class DigitalRoot {
+    /**
+     *problem link
+     * https://www.codewars.com/kata/541c8630095125aba6000c00
+     *
+     * @param n
+     * @return
+     */
+    public static int digital_root(int n) {
+        if (n < 10)
+            return n;
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        if (sum > 9)
+            sum = digital_root(sum);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int val = digital_root(132189); //132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+        System.out.println(val);
+    }
+}
